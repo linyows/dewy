8b03867a338b97db0984630e864ed92099401924
linyows <linyows@gmail.com>
2018-02-18 23:39:56 +0900

    add interval for config

--- config.go
+++ config.go
@@ -1,10 +1,13 @@
 package dewy
 
 import (
+	"time"
+
 	starter "github.com/lestrrat-go/server-starter"
 )
 
 type Config struct {
 	Repository          string
+	Interval            time.Duration
 	ServerStarterConfig starter.Config
 }
