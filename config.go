93f292bea87d33181670d53c049fed65ab99386e
linyows <linyows@gmail.com>
2018-02-26 00:19:29 +0900

    split config

--- config.go
+++ config.go
@@ -1,13 +1,72 @@
 package dewy
 
 import (
-	"time"
+	"os"
 
 	starter "github.com/lestrrat-go/server-starter"
 )
 
+type CacheType int
+
+const (
+	NONE CacheType = iota
+	FILE
+)
+
+func (c CacheType) String() string {
+	switch c {
+	case NONE:
+		return "none"
+	case FILE:
+		return "file"
+	default:
+		return "unknown"
+	}
+}
+
+type CacheConfig struct {
+	Type       CacheType
+	Expiration int
+}
+
+type RepositoryProvider int
+
+const (
+	GITHUB RepositoryProvider = iota
+)
+
+func (r RepositoryProvider) String() string {
+	switch r {
+	case GITHUB:
+		return "github"
+	default:
+		return "unknown"
+	}
+}
+
+type RepositoryConfig struct {
+	Provider RepositoryProvider
+	Owner    string
+	Name     string
+	Token    string
+	Endpoint string
+}
+
 type Config struct {
-	Repository          string
-	Interval            time.Duration
-	ServerStarterConfig starter.Config
+	Repository RepositoryConfig
+	Cache      CacheConfig
+	Starter    starter.Config
+}
+
+func (c *Config) OverrideWithEnv() {
+	if c.Repository.Provider == GITHUB {
+		githubToken := os.Getenv("GITHUB_TOKEN")
+		if githubToken != "" {
+			c.Repository.Token = githubToken
+		}
+		githubEndpoint := os.Getenv("GITHUB_ENDPOINT")
+		if githubEndpoint != "" {
+			c.Repository.Endpoint = githubEndpoint
+		}
+	}
 }
