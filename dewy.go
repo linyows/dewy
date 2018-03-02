0b3d48a792a12a7b78adf9e1ae1412a409b12dd8
linyows <linyows@gmail.com>
2018-03-03 00:20:02 +0900

    split dewy

--- dewy.go
+++ dewy.go
@@ -1,59 +1,89 @@
-//package dewy
-package main
+package dewy
 
 import (
 	"context"
 	"fmt"
 	"net/url"
-	"os"
-	"runtime"
 
-	"github.com/carlescere/scheduler"
 	"github.com/google/go-github/github"
+	"github.com/lestrrat-go/server-starter"
 	"golang.org/x/oauth2"
 )
 
-func main() {
-	job := release()
-	scheduler.Every(10).Seconds().NotImmediately().Run(job)
-	runtime.Goexit()
+type Repository struct {
+	provider RepositoryProvider
+	token    string
+	endpoint string
+	owner    string
+	name     string
 }
 
-func client(ctx context.Context, token string, endpoint string) *github.Client {
-	if token == "" {
-		token = os.Getenv("GITHUB_TOKEN")
+func NewRepository(c RepositoryConfig) *Repository {
+	return &Repository{
+		provider: c.Provider,
+		token:    c.Token,
+		endpoint: c.Endpoint,
+		owner:    c.Owner,
+		name:     c.Name,
 	}
+}
+
+func (r *Repository) Fetch() error {
+	ctx := context.Background()
+	c, err := r.client(ctx)
+	if err != nil {
+		return err
+	}
+	release, _, err := c.Repositories.GetLatestRelease(ctx, r.owner, r.name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Assets: %d\n", len(release.Assets))
+	for _, v := range release.Assets {
+		fmt.Printf("%s -- Size: %d, Download: %d <%s>\n",
+			*v.Name, *v.Size, *v.DownloadCount, *v.BrowserDownloadURL)
+	}
+	return nil
+}
+
+func (r *Repository) client(ctx context.Context) (*github.Client, error) {
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
+		&oauth2.Token{AccessToken: r.token},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	if endpoint != "" {
-		url, err := url.Parse(endpoint)
+	if r.endpoint != "" {
+		url, err := url.Parse(r.endpoint)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		client.BaseURL = url
 	}
 
-	return client
+	return client, nil
 }
 
-func release() func() {
-	return func() {
-		ctx := context.Background()
-		owner := "linyows"
-		repo := "octopass"
-		c := client(ctx, "", "")
-		release, _, err := c.Repositories.GetLatestRelease(ctx, owner, repo)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Assets: %d\n", len(release.Assets))
-		for _, v := range release.Assets {
-			fmt.Printf("%s -- Size: %d, Download: %d <%s>\n",
-				*v.Name, *v.Size, *v.DownloadCount, *v.BrowserDownloadURL)
-		}
+type Cache struct {
+}
+
+type Dewy struct {
+	config     Config
+	repository Repository
+	cache      Cache
+	starter    starter.Config
+}
+
+func New(c Config) *Dewy {
+	return &Dewy{
+		config: c,
 	}
+}
+
+func (d *Dewy) Run() error {
+	r := NewRepository(d.config.Repository)
+	if err := r.Fetch(); err != nil {
+		return err
+	}
+	return nil
 }
