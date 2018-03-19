0bf0fce9ff20d56c03d08a6c4d9b145b96579d35
linyows <linyows@gmail.com>
2018-03-20 01:47:39 +0900

    add download method

--- repository.go
+++ repository.go
@@ -2,7 +2,11 @@ package dewy
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"os"
+	"path"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -46,6 +50,28 @@ func (r *Repository) Fetch() error {
 		}
 		//fmt.Printf("%s -- Size: %d, Download: %d <%s>\n", *v.Name, *v.Size, *v.DownloadCount, *v.BrowserDownloadURL)
 	}
+	return nil
+}
+
+func (r *Repository) Download() error {
+	res, err := http.Get(r.downloadURL)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	_, filename := path.Split(r.downloadURL)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.Write(body)
+
 	return nil
 }
 
