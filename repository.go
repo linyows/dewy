64adf2e1cfe27f45cb360ebdb68ca585f8a82caa
linyows <linyows@gmail.com>
2018-03-21 23:16:01 +0900

    add repository interfaces

--- repository.go
+++ repository.go
@@ -12,8 +12,12 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type Repository struct {
-	provider    RepositoryProvider
+type Repository interface {
+	Fetch() error
+	Download() error
+}
+
+type GithubReleaseRepository struct {
 	token       string
 	endpoint    string
 	owner       string
@@ -22,30 +26,34 @@ type Repository struct {
 	downloadURL string
 }
 
-func NewRepository(c RepositoryConfig) *Repository {
-	return &Repository{
-		provider: c.Provider,
-		token:    c.Token,
-		endpoint: c.Endpoint,
-		owner:    c.Owner,
-		name:     c.Name,
-		artifact: c.Artifact,
+func NewRepository(c RepositoryConfig) Repository {
+	switch c.Provider {
+	case GITHUB:
+		return &GithubReleaseRepository{
+			token:    c.Token,
+			endpoint: c.Endpoint,
+			owner:    c.Owner,
+			name:     c.Name,
+			artifact: c.Artifact,
+		}
+	default:
+		panic("no repository provider")
 	}
 }
 
-func (r *Repository) Fetch() error {
+func (g *GithubReleaseRepository) Fetch() error {
 	ctx := context.Background()
-	c, err := r.client(ctx)
+	c, err := g.client(ctx)
 	if err != nil {
 		return err
 	}
-	release, _, err := c.Repositories.GetLatestRelease(ctx, r.owner, r.name)
+	release, _, err := c.Repositories.GetLatestRelease(ctx, g.owner, g.name)
 	if err != nil {
 		return err
 	}
 	for _, v := range release.Assets {
-		if *v.Name == r.artifact {
-			r.downloadURL = *v.BrowserDownloadURL
+		if *v.Name == g.artifact {
+			g.downloadURL = *v.BrowserDownloadURL
 			break
 		}
 		//fmt.Printf("%s -- Size: %d, Download: %d <%s>\n", *v.Name, *v.Size, *v.DownloadCount, *v.BrowserDownloadURL)
@@ -53,8 +61,8 @@ func (r *Repository) Fetch() error {
 	return nil
 }
 
-func (r *Repository) Download() error {
-	res, err := http.Get(r.downloadURL)
+func (g *GithubReleaseRepository) Download() error {
+	res, err := http.Get(g.downloadURL)
 	if err != nil {
 		return err
 	}
@@ -64,7 +72,7 @@ func (r *Repository) Download() error {
 		return err
 	}
 
-	_, filename := path.Split(r.downloadURL)
+	_, filename := path.Split(g.downloadURL)
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
@@ -75,15 +83,15 @@ func (r *Repository) Download() error {
 	return nil
 }
 
-func (r *Repository) client(ctx context.Context) (*github.Client, error) {
+func (g *GithubReleaseRepository) client(ctx context.Context) (*github.Client, error) {
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: r.token},
+		&oauth2.Token{AccessToken: g.token},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	if r.endpoint != "" {
-		url, err := url.Parse(r.endpoint)
+	if g.endpoint != "" {
+		url, err := url.Parse(g.endpoint)
 		if err != nil {
 			return nil, err
 		}
