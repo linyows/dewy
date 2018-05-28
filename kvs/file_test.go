1dc88f107973c6e7252524b719f505457348384d
linyows <linyows@gmail.com>
2018-05-29 03:43:31 +0900

    add test

--- kvs/file_test.go
+++ kvs/file_test.go
@@ -1,6 +1,9 @@
 package kvs
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -19,4 +22,59 @@ func TestFileDefault(t *testing.T) {
 	if isFileExist(f.dir) != true {
 		t.Error("file dir expects not setted")
 	}
+	defer os.RemoveAll(f.dir)
+}
+
+func TestFileWrite(t *testing.T) {
+	f := &File{}
+	f.Default()
+	data := []byte("this is data for test")
+	err := f.Write("test", data)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if isFileExist(filepath.Join(f.dir, "test")) != true {
+		t.Error("file not found for cache")
+	}
+	defer os.RemoveAll(f.dir)
+}
+
+func TestFileDelete(t *testing.T) {
+	f := &File{}
+	f.Default()
+	data := []byte("this is data for test")
+	err := f.Write("testdelete", data)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	p := filepath.Join(f.dir, "testdelete")
+	if isFileExist(p) != true {
+		t.Error("file not created")
+	}
+	err = f.Delete("testdelete")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if isFileExist(p) != false {
+		t.Error("file not deleted")
+	}
+	defer os.RemoveAll(f.dir)
+}
+
+func TestFileList(t *testing.T) {
+	f := &File{}
+	f.Default()
+	data := []byte("this is data for test")
+	err := f.Write("testlist", data)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	list, err := f.List()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if fmt.Sprintf("%#v", list) != "[]string{\"testlist\"}" {
+		t.Error("return is not correct")
+	}
+	defer os.RemoveAll(f.dir)
 }
