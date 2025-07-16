01f0482ee49169dcfec8cc0c8ec23ccdf1e7548f
linyows <linyows@gmail.com>
2025-07-16 14:27:15 +0900

    update banner

--- banner.go
+++ banner.go
@@ -8,8 +8,10 @@ import (
 // Banner displays the Dewy ASCII art logo
 func Banner(w io.Writer) {
 	fmt.Fprint(w, `
-█▀▄ █▀▀ █ █ █ █
-█ █ █▀▀ █▄█ ▀█▀
-▀▀  ▀▀▀  ▀   ▀ 
+ ___   ___  _____ __  __
+|   \ | __| \  / /\ \/ /
+|   | | __| \ / /  \  /
+|___/ |___| \_/_/  |__|
+
 `)
 }
